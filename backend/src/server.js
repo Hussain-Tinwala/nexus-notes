1d826c85ee189428d2ceba9cb107638fc69c1fa4
Hussain Tinwala <coder.hussaintin@gmail.com>
2026-03-07 21:38:32 +0530

    chore: setup database connection and integrate MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,17 @@
 import express from "express"
 import notesRoutes from "./routes/notes.routes.js"
 import { createNote, deleteNote, getAllNotes, updateNote } from "./contollers/notes.controller.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config();
+const PORT=process.env.PORT || 5001;
 
 const app=express()
+connectDB();
+
 app.use('/api/notes', notesRoutes);
 
 app.listen(5001, ()=>{
     console.log("Server running on PORT: 5001");
 })
-app.get(5001,getAllNotes)
-app.post(5001,createNote)
-app.put(5001,updateNote)
-app.delete(5001,deleteNote)
