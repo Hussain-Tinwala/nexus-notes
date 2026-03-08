7d2965f272b264db49e8ec25c8ac7354a38183cc
Hussain Tinwala <coder.hussaintin@gmail.com>
2026-03-08 21:45:11 +0530

    fix(middleware): increased the rate limit to solve this issue

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,19 +1,20 @@
 import ratelimit from "../config/upstash.js";
 
-const rateLimiter=async(req, res, next)=>{
+const rateLimiter = async (req, res, next) => {
     try {
-        const {success}=await ratelimit.limit("my-limit-key")
+        const identifier = req.ip || "global"; 
 
-        if(!success){
-            return res.status(429).json({message:"Too many requests"});
+        const { success } = await ratelimit.limit(identifier);
+
+        if (!success) {
+            return res.status(429).json({ message: "Too many requests" });
         }
         
-        next()
+        next();
     } catch (error) {
-        console.error("Rate Limit Error", error)
-        next(error)
+        console.error("Rate Limit Error", error);
+        next(); 
     }
-
 }
 
 export default rateLimiter
